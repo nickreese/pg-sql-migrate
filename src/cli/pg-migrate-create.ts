2ac9253f9270ecca0d8ecbdafe08b4d871355452
Nick Reese <nick@nicholasreese.com>
2020-03-06 17:28:26 -0500

    use .sql files

--- src/cli/pg-migrate-create.ts
+++ src/cli/pg-migrate-create.ts
@@ -9,7 +9,7 @@ new Command()
   .option('-c, --config <path>', 'Path to the configuration file', DEFAULT_CONFIG_FILE)
   .action(async (name: string, content: string | undefined, { config }: { config?: string }) => {
     const { directory } = loadConfig(config);
-    const file = `${new Date().toISOString()}_${name}.pgsql`;
+    const file = `${new Date().toISOString()}_${name}.sql`;
 
     writeFileSync(join(directory, file), content || '');
 
